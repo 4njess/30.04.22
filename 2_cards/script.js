8f1253868dd267946a45faf427baffa509b551b5
Artem Nefedov <artemnefedov777@gmail.com>
2022-05-07 11:35:41 +0500

    Add files via upload

--- 2_cards/script.js
+++ 2_cards/script.js
@@ -1,47 +1,68 @@
 const game = {
-    height: 198,
-    width: 320,
-    matrix: 16,
-    cards: [
-        {
-            color: '#87D204',
-            isShow: false,
-        },
-        {
-            color: '#D27904',
-            isShow: false,
-        },
-        {
-            color: '#87D204',
-            isShow: false,
-        },
-        {
-            color: '#D27904',
-            isShow: false,
-        },
-    ]
+    height: 198 * .61834,
+    width: 320 * .61834,
+    matrix: 6,
+    cards: [],
+    firstCard: null,
+    getCardById: (cards, cardId) => {
+        let card = null;
+        cards.map(c => {
+            if (c.id === cardId){
+                card = c;
+            }
+        });
+        return card;
+    },
+    setIsShowCards:(cards, arrayCardId) => {
+        cards.map( c => {
+            for (let i = 0; i < arrayCardId.length; i++) {
+                const cardId = arrayCardId[i];
+                if (c.id === cardId){
+                    c.iShow = true;
+                }
+                
+            }
+        });
+        
+        return cards;
+    }
 }
 
 game.cards = generateCards(game.matrix);
 
 function generateCards(matrix) {
-    const cards = [];
-    const countCards = (matrix * matrix) / 2;
+    const colors = [];
+    const countColors = (matrix * matrix) / 2;
 
-    for (let i = 0; i < countCards; i++) {
-        const e = {
-            color: randomColorRGB(),
-            isShow: false, 
-
+    for (let i = 0; i < countColors; i++) {
+            const color = randomColorRGB();
+            colors.push(color);
+            colors.push(color);
         }
-        cards.push(e);
+
+  
+
+    const cards = [];
+    for (let i = 0; i < colors.length; i++) {
+        const card = {
+            id: i,
+            color: colors[i],
+            iShow: false
+        }
+        
+        cards.push(card);
         
     }
 
+    for (let i = 0; i < 64; i++) {
+        cards.sort(() => Math.random() - 0,5);
 
+    }
+    return cards;
+    
     return sortArray([...cards, ...cards]);
 
-}
+    }
 
     function sortArray(array){
         for (let i = 0; i < 64; i++) {
@@ -70,17 +91,69 @@ $(document).ready(function () {
 
     for (let i = 0; i < game.cards.length; i++) {
         $(".matrix").append("<div id='cube_" + i + "'class ='cube'> </div>");
-    }
+        
+        $("#cube_" + i).css({
+        "width":game.width,
+        "height":game.height,
+        "background-color" : '#1A1A1A'
+    });
+}
+
 
     for (let i = 0; i < game.cards.length; i++) {
         const e = game.cards[i];
         $("#cube_" + i).click(function () {
             const id = $("#cube_" + i).attr('id');
 
+
             $("#cube_" + i).css({
                 "background-color": e.color
             });
             console.log(Number(id.replace('cube_', '')));
+
+
+            const card = game.getCardById(
+                game.cards,
+                Number(id.replace('cube_', '')) 
+                
+
+            );
+
+            console.log(card);
+
+            if (!game.firstCard){
+                game.firstCard = card;
+
+            }
+            else if(!!game.firstCard) {
+                if(game.firstCard.color === card.color){
+                    
+                    game.cards = game.setIsShowCards(
+                        game.cards,
+                        [game.firstCard.id, card.id]
+                        );
+                    game.firstCard = null; 
+                    
+                }
+                else {
+                    setTimeout(() => {
+                    hideCard(game.firstCard.id);
+                    hideCard(card.id);
+                    game.firstCard = null;
+                    }, 100);
+
+                }
+            }
+        });
+    }
+
+
+
+
+
+    function hideCard(cardId) {
+        $("#cube_" + cardId).css({
+            "background-color": "#1A1A1A"
         });
     }
 });
